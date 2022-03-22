68cccd43ec7ef6183679cbb15f2336247dc88ea1
winwreck <jdr85@duke.edu>
2022-03-22 12:18:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ server = app.server
 app.layout = html.Div([
     html.H1('You Passed'),
     github_info_header(),
-    html.Img(src="assets/cute_ferret.jpeg")
+    html.Img(src="assets/burb.jpeg")
 ])
 
 if __name__ == '__main__':
